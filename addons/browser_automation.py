f76dd73da7d74b6fcba59c5404e20fdf457d58e4
Prakashmaheshwaran <zskillhelps@gmail.com>
2024-05-23 23:35:25 +0530

    time adjustment done

--- addons/browser_automation.py
+++ addons/browser_automation.py
@@ -42,19 +42,19 @@ def perform_steps(url: str, tag: str, keyword: str, seo_title: str, meta_descrip
         print(f"Opening URL: {url}")
         driver.get(url)
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
-        time.sleep(1.5)
+        time.sleep(0.5)
 
         print("Filling in the tag.")
         tag_xpath = '//*[@id="components-form-token-input-0"]'
         tag_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, tag_xpath)))
         tag_element.send_keys(tag)
-        time.sleep(1.5)
+        time.sleep(0.5)
 
         print("Filling in the keyword.")
         keyword_xpath = '//*[@id="focus-keyword-input-metabox"]'
         keyword_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, keyword_xpath)))
         keyword_element.send_keys(keyword)
-        time.sleep(1.5)
+        time.sleep(0.5)
 
         print("Filling in the SEO title.")
         seo_title_xpath = '//*[@id="yoast-google-preview-title-metabox"]/div/div/div'
@@ -62,9 +62,9 @@ def perform_steps(url: str, tag: str, keyword: str, seo_title: str, meta_descrip
         seo_title_element.click()
         time.sleep(0.5)
         seo_title_element.send_keys('\b' * 15)  # Press backspace 15 times
-        time.sleep(1.5)
+        time.sleep(0.5)
         seo_title_element.send_keys(seo_title)
-        time.sleep(1.5)
+        time.sleep(0.5)
 
         print("Filling in the meta description.")
         meta_description_xpath = '//*[@id="yoast-google-preview-description-metabox"]/div/div/div'
@@ -76,7 +76,7 @@ def perform_steps(url: str, tag: str, keyword: str, seo_title: str, meta_descrip
         button_xpath = '//*[@id="editor"]/div[1]/div[1]/div[1]/div/div[3]/button'
         button_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, button_xpath)))
         button_element.click()
-        time.sleep(1.5)
+        time.sleep(8)
         
     except Exception as e:
         print(f"An error occurred: {e}")
